0a13969f906c5fb72f076ac20d9c38e886066a29
Mumo23 <147050720+Mumo23@users.noreply.github.com>
2024-02-29 18:49:12 +0300

    Added stuff

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState();
+export default function Weather(props) {
+  const [forecast, setForecast] = useState({ ready: false });
 
   function newWeather(response) {
-    setTemperature(response.data.main.temp);
+    setForecast({
+      ready: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      wind: response.data.wind.speed,
+      humdity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: response.data.weather[0].icon,
+    });
   }
 
-  if (ready) {
+  if (forecast.ready) {
     return (
-      <div className="app">
+      <div className="waether-app">
         <div className="form">
           <form id="search-form">
             <input
@@ -27,49 +34,40 @@ export default function Weather() {
         <main>
           <div className="app-data">
             <div>
-              <h1 className="city"> Nairobi </h1>
+              <h1 className="city"> {forecast.city} </h1>
               <p>
                 <span className="current-date" id="current-date">
                   Tuesday 11:55am{" "}
                 </span>
-                <br />
-                Humidity:
-                <span className="current-humidity" id="humidity"></span>
-                Wind:<span className="wind-speed" id="wind-speed"></span>
+
+                <ul>
+                  <li>Temperature: {Math.round(forecast.temperature)}°C</li>
+                  <li>Description: {forecast.description}</li>
+                  <li>Humidity: {forecast.humidity}%</li>
+                  <li>Wind: {forecast.wind}Km/H</li>
+                  <li>
+                    <img src={forecast.icon} alt={forecast.description} />
+                  </li>
+                </ul>
               </p>
             </div>
 
             <div className="app-temperature">
               <div className="icon" id="icon"></div>
               <div className="current-temperature" id="current-temperature">
-                12
+                {Math.round(forecast.temperature)}
               </div>
               <div className="unit">°C</div>
             </div>
           </div>
         </main>
 
-        <div className="weather-forecast" id="forecast"></div>
-
-        <footer>
-          <p>
-            This project was designed & coded by
-            <a href="https://www.linkedin.com/in/miriam-mumo-6a0950b3/">
-              Mumo Mailu
-            </a>
-            and is
-            <a href="https://github.com/Mumo23/mumo-weatherapp-project">
-              on GitHub
-            </a>
-            and hosted on
-            <a href="https://app.netlify.com/user/settings#profile">Netlify</a>
-          </p>
-        </footer>
+        <div className="weather-forecast"></div>
       </div>
     );
   } else {
     const apiKey = "49eb13bf4a9b386162a0657c95c63c29";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultcity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(newWeather);
 
     return "Loading..";
