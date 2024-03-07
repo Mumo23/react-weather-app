1a3eb9259d1100dd22700da932d139bcd87982be
Mumo23 <147050720+Mumo23@users.noreply.github.com>
2024-03-07 21:32:11 +0300

    Created EntireForecastDay.js component

--- src/EntireForecast.js
+++ src/EntireForecast.js
@@ -1,29 +1,33 @@
-import React from "react";
-import Icon from "./Icon";
+import React, { useState } from "react";
 import axios from "axios";
+import EntireForecastDay from "./EntireForecastDay.js";
 
 export default function EntireForecast(props) {
-  function newWeather(response) {}
+  let [ready, setReady] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-  let apiKey = "49eb13bf4a9b386162a0657c95c63c29";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}
-&units=metric`;
-  axios.get(apiUrl).then(newWeather);
+  function newForecast(response) {
+    setForecast(response.data.daily);
+    setReady(true);
+  }
 
-  return (
-    <div className="EntireForecast">
-      <div className="row">
-        <div className="col">
-          <div className="EntireForecast-day">Thur</div>
-          <Icon code="01d" size={24} />
-          <div className="EntireForecast-temperature">
-            <span className="EntireForecast-max">19°</span>
-            <span className="EntireForecast-min">17°</span>
+  if (ready) {
+    return (
+      <div className="EntireForecast">
+        <div className="row">
+          <div className="col">
+            <EntireForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "49eb13bf4a9b386162a0657c95c63c29";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(newForecast);
+
+    return null;
+  }
 }
---
+++ src/EntireForecastDay.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Icon from "./Icon";
+
+export default function EntireForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="EntireForecast-day">{day()}</div>
+      <Icon code={props[0].weather[0].icon} size={24} />
+      <div className="EntireForecast-temperature">
+        <span className="EntireForecast-max">{maxTemperature()}</span>
+        <span className="EntireForecast-min">{minTemperature()}</span>
+      </div>
+    </div>
+  );
+}
